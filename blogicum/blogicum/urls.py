e0c05874be94800e7bbf8b03ec5109faf42f7eac
Павел Филиппов <pgphil@yandex.ru>
2023-04-10 08:25:42 +0300

    Forth version Correct general url and teg title.

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls', namespace='pages')),
+    path('pages/', include('pages.urls', namespace='pages')),
     path('', include('blog.urls', namespace='index')),
 ]
